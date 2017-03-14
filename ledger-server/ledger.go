d26ba6755206d7738b828980245fb132e61998f9
Calum Gardner <calum@qubit.com>
2017-03-14 17:54:54 +0000

    stuff

--- ledger-server/ledger.go
+++ ledger-server/ledger.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"strconv"
-	"golang.org/x/net/context"
 	_ "github.com/golang/protobuf/proto"
-	pb "../proto"
+	pb "github.com/jackgardner/go-ledger/proto"
+	"golang.org/x/net/context"
+	"strconv"
 )
 
 type LedgerServer struct {
@@ -12,14 +12,14 @@ type LedgerServer struct {
 }
 
 func (s *LedgerServer) CreateTransaction(ctx context.Context, request *pb.CreateTransactionRequest) (*pb.Transaction, error) {
-	var transactionId = strconv.Itoa(len(s.Transactions) + 1);
-	var newTransaction = &pb.Transaction{Success: true, TransactionId: transactionId, AmountInPence: request.AmountInPence }
+	var transactionId = strconv.Itoa(len(s.Transactions) + 1)
+	var newTransaction = &pb.Transaction{Success: true, TransactionId: transactionId, AmountInPence: request.AmountInPence}
 	s.Transactions[transactionId] = newTransaction
 	return newTransaction, nil
 }
 
 func (s *LedgerServer) GetTransaction(ctx context.Context, request *pb.GetTransactionRequest) (*pb.Transaction, error) {
-	return s.Transactions[request.TransactionId], nil;
+	return s.Transactions[request.TransactionId], nil
 }
 
 func (s *LedgerServer) GetTransactions(ctx context.Context, request *pb.ListTransactionsRequest) (*pb.TransactionsReply, error) {
@@ -32,6 +32,6 @@ func (s *LedgerServer) GetTransactions(ctx context.Context, request *pb.ListTran
 
 	return &pb.TransactionsReply{
 		Transactions: values,
-		LedgerId: request.SourceLedgerId,
-	}, nil;
+		LedgerId:     request.SourceLedgerId,
+	}, nil
 }
